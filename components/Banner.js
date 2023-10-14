9f4c706d28161efea824ab04eed6b8b010065b46
Yip Zhang <yipliangzhang@hotmail.com>
2023-10-14 17:24:46 +0200

    sd

--- components/Banner.js
+++ components/Banner.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import neonLogo from "../public/assets/images/Purple_Neon_Drip_Logo.svg";
 import { useState, useEffect } from "react";
+import gsap from "gsap";
+
 
 function Banner() {
     const [isBouncing, setIsBouncing] = useState(false);
 
     return (
         <div className="flex flex-col h-screen bg-main-black items-center justify-center">
-            <h1 className="font-akira text-2xl md:text-5xl lg:text-6xl xl:text-7xl text-white mb-10 mt-52 ">
+            <h1 className="font-akira text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-white mb-10 mt-52 ">
                 Scrollium
             </h1>
 
@@ -19,7 +21,7 @@ function Banner() {
             rel="noopener noreferrer"
             className="flex justify-center md:justify-normal"
         >
-            <button className="font-akira text-xl md:text-3xl lg:text-5xl text-white px-5 py-1 border-b-2 mt-10 bounce-in">
+            <button className="font-akira text-2xl md:text-3xl lg:text-5xl text-white px-5 py-1 border-b-2 mt-10 bounce-in">
                 Raffle
             </button>
         </a>
@@ -31,7 +33,7 @@ function Banner() {
             rel="noopener noreferrer"
             className="flex justify-center md:justify-normal"
         >
-            <button className="font-akira text-xl md:text-3xl lg:text-5xl text-white px-5 md:px-12 py-1 border-b-2 2xl:ml-48 mt-10 bounce-in line-through">
+            <button className="font-akira text-2xl md:text-3xl lg:text-5xl text-white px-5 md:px-12 py-1 border-b-2 md:ml-16 xl:ml-32 mt-10 bounce-in line-through">
                 Prophex
             </button>
         </a>
@@ -46,7 +48,7 @@ function Banner() {
                 rel="noopener noreferrer"
                 className="mb-10 mt-5 translate-x-5 5xl:translate-x-4"
             >
-                <button className="font-akira text-2xl md:text-6xl text-white border-b-2 pb-1 mr-10 mt-10 bounce-in">
+                <button className="font-akira text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl text-white border-b-2 pb-1 mr-10 mt-10 bounce-in">
                     Pass
                 </button>
             </a>
