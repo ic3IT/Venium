be50ab941b9b20fdca0c2e0c953537cf29a79285
Yip Zhang <yipliangzhang@hotmail.com>
2023-10-14 17:33:54 +0200

    banner fix

--- components/Banner.js
+++ components/Banner.js
@@ -13,8 +13,8 @@ function Banner() {
                 Scrollium
             </h1>
 
-            <div className="flex flex-wrap  gap-32 w-full mb-10 px-4 justify-center items-center">
-    <div className="col-start-1 sm:col-start-1 md:col-start-2 lg:col-start-2 xl:col-start-2 2xl:col-start-2">
+            <div className="flex flex-wrap gap-24 md:gap-32 w-full mb-10 px-4 justify-center items-center">
+    <div className="">
         <a
             href="https://raffle.scrollium.io/"
             target="_blank"
@@ -26,14 +26,14 @@ function Banner() {
             </button>
         </a>
     </div>
-    <div className="col-start-1 sm:col-start-2 md:col-start-3 lg:col-start-3 xl:col-start-3 2xl:col-start-3">
+    <div className="">
         <a
             href="https://prophex.scrollium.io/"
             target="_blank"
             rel="noopener noreferrer"
             className="flex justify-center md:justify-normal"
         >
-            <button className="font-akira text-2xl md:text-3xl lg:text-5xl text-white px-5 md:px-12 py-1 border-b-2 md:ml-16 xl:ml-32 mt-10 bounce-in line-through">
+            <button className="font-akira text-2xl md:text-3xl lg:text-5xl text-white px-5 md:px-12 py-1 border-b-2 md:ml-16 xl:ml-48 md:mt-10 bounce-in line-through">
                 Prophex
             </button>
         </a>
@@ -48,7 +48,7 @@ function Banner() {
                 rel="noopener noreferrer"
                 className="mb-10 mt-5 translate-x-5 5xl:translate-x-4"
             >
-                <button className="font-akira text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl text-white border-b-2 pb-1 mr-10 mt-10 bounce-in">
+                <button className="font-akira text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl text-white border-b-2 pb-1 mr-10 mt-10 bounce-in">
                     Pass
                 </button>
             </a>
