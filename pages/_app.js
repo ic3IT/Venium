1e1821b455383dbf681e29a94d05273a134e10b4
Yip Zhang <yipliangzhang@hotmail.com>
2024-01-10 01:21:58 +0100

    metadata

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,24 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Venium</title>
-        <meta name="description" content="Scrollium" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta charset="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Venium: Innovative Community Ecosystem on Scroll zkEVM" />
+        
+        <meta property="og:title" content="Venium: Innovative Community Ecosystem on Scroll zkEVM" />
+        <meta property="og:description" content="Venium: Innovative Community Ecosystem on Scroll zkEVM - Dive in and experience innovation at its finest." />
+        <meta property="og:image" content="https://i.imgur.com/grPbAkO.png" />
+        <meta property="og:url" content="https://venium.io" />
+
+        <meta name="twitter:card" content="summary_large_image "/>
+        <meta name="twitter:title" content="Venium: Innovative Community Ecosystem on Scroll zkEVM"/>
+        <meta name="twitter:description" content="Venium: Innovative Community Ecosystem on Scroll zkEVM - Dive in and experience innovation at its finest."/>
+        <meta name="twitter:image" content="https://i.imgur.com/grPbAkO.png"/>
+
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
       </Head>
 
       <Component {...pageProps} />
